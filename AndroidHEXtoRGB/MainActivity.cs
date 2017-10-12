fc29fdd81c99d22c1ffbad0d8eae0859110306c0
mattyDunn <31864601+mattyDunn@users.noreply.github.com>
2017-10-12 13:47:29 -0600

    cool

---
+++ AndroidHEXtoRGB/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Views;
+
+namespace AndroidHEXtoRGB
+{
+    [Activity(Label = "AndroidHEXtoRGB", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+
+        Button convertHexButton;
+        TextView redTextView, greenTextView, blueTextView;
+        EditText hexValueEditText;
+        View colorView;
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            hexValueEditText = FindViewById<EditText>(Resource.Id.
+
+        }
+    }
+}
+
